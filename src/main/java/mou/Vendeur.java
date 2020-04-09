d8498296c446b6406ba86ce3886967f8ebd018c2
uvsq21807189 <mouwafak.houssein.ahmed@gmail.com>
2020-04-09 10:21:14 +0200

    mise à jour de la classe vendeur

--- src/main/java/mou/Vendeur.java
+++ src/main/java/mou/Vendeur.java
@@ -1,18 +1,20 @@
 package mou;
 
-
 public class Vendeur extends SalaireTotal {
 
   private int commission;
+  private int anc;
   
-  public Vendeur(int commission) {
+  public Vendeur(int commission, int anc) {
 
     this.commission=commission;
-   
+    this.anc=anc;
 }
+  @Override
   
   public double salaire() {
    
-    return salaire_base+commission;}
+    return salaire_base+commission+20*anc;}
   }
+
 

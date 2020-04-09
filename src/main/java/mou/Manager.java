3b0841ee62c25a9e93f839ad14dd0ac032f051d8
uvsq21807189 <mouwafak.houssein.ahmed@gmail.com>
2020-04-09 10:23:52 +0200

    mise à jour de la classe Manager

--- src/main/java/mou/Manager.java
+++ src/main/java/mou/Manager.java
@@ -1,18 +1,21 @@
 package mou;
 
-
 public class Manager extends SalaireTotal {
   
   private int nbrEmploye;
+  private int anc;
   
-  public Manager(int nbrEmploye) {
+  public Manager(int nbrEmploye, int anc) {
     
     this.nbrEmploye=nbrEmploye;
-   }
+    this.anc=anc;
+  }
+
+  @Override
   
   public double salaire() {
     
-    return salaire_base+ (nbrEmploye*100);
+    return salaire_base+(anc*20)+ (nbrEmploye*100);
   }
 }
 
